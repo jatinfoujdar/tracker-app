ec8f64f79be98c92bbe60303fe5b279cc8c5031d
jatin foujdar <jatin321001@gmail.com>
2022-12-02 17:32:52 +0530

    setup done commit

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+require("dotenv").config()
 const express = require('express')
 const app = express()
 const port = 4000
@@ -6,6 +7,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+
+
+
+module.exports = app
